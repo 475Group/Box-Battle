9136ff1e0b431ce638174990db5230376efc3a9d
JadaFranklin <franklinja@vcu.edu>
2022-02-28 22:24:44 -0500

    Made a functional StartScreen

---
+++ app/src/main/java/com/example/myapplication/StartScreen.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+      private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+
+            }
+        });
+
+
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
