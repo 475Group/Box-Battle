6879f57fae5153b0353d8e9ab6192de692832272
JadaFranklin <franklinja@vcu.edu>
2022-03-22 13:42:29 -0400

    Remove title bar and action bar from StartScreen

--- app/src/main/java/com/example/myapplication/StartScreen.java
+++ app/src/main/java/com/example/myapplication/StartScreen.java
@@ -5,16 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
-      private Button button;
+     Button button;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+         //Remove title bar
+         requestWindowFeature(Window.FEATURE_NO_TITLE);
+         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+         //Hides action bar
+         if(getSupportActionBar() != null){
+             getSupportActionBar().hide();
+         }
+
         setContentView(R.layout.activity_start_screen);
+
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
