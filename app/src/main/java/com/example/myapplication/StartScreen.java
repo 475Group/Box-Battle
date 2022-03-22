a3073df2340f544fb04220e10ea25089662d5b7d
JadaFranklin <franklinja@vcu.edu>
2022-03-22 13:09:51 -0400

    Update StartScreen

--- app/src/main/java/com/example/myapplication/StartScreen.java
+++ app/src/main/java/com/example/myapplication/StartScreen.java
@@ -21,6 +21,7 @@ public class StartScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openMainActivity();
+                finish();
 
             }
         });
