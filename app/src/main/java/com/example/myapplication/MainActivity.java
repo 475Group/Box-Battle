f839a182b8a1b9cf24cda30dcadfcbe97ad533f0
khadija <khalifak2@vcu.edu>
2022-03-28 17:41:12 -0400

    added custom player color functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,6 +22,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView player1Score, player2Score, player1Text, player2Text, player1Turn, player2Turn;
     String player1Name, player2Name;
+    int player1Color, player2Color;
+
     int player1ScoreValue = 0, player2ScoreValue = 0;
     View previousLine = null;
     int turn = 0;
@@ -32,10 +34,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void setUpPlayerInfo(){
         if (!(player1Name.equals(""))) {
             player1Text.setText(player1Name);
+
         }
+        player1Text.setTextColor(player1Color);
+        player1Score.setTextColor(player1Color);
+
         if (!(player2Name.equals(""))) {
             player2Text.setText(player2Name);
         }
+        player2Text.setTextColor(player2Color);
+        player2Score.setTextColor(player2Color);
+
     }
 
     @Override
@@ -50,16 +59,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             getSupportActionBar().hide();
         }
 
+
         setContentView(R.layout.activity_game_page);
         player1Text = findViewById(R.id.player1Text);
         player1Name = getIntent().getExtras().getString("P1");
+        player1Color = getIntent().getIntExtra("colorP1", -16777216);
         player2Text = findViewById(R.id.player2Text);
         player2Name = getIntent().getExtras().getString("P2");
-        setUpPlayerInfo();
+        player2Color = getIntent().getIntExtra("colorP2", -16777216);
         player1Score = findViewById(R.id.player1Score);
         player2Score = findViewById(R.id.player2Score);
         player1Turn = findViewById(R.id.Player1Turn);
         player2Turn = findViewById(R.id.Player2Turn);
+        setUpPlayerInfo();
         ImageView settingsButton = findViewById(R.id.settingsIcon);
         ImageView infoButton = findViewById(R.id.infoIcon);
 
--- app/src/main/java/com/example/myapplication/SetUpScreenActivity.java
+++ app/src/main/java/com/example/myapplication/SetUpScreenActivity.java
@@ -8,15 +8,23 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class SetUpScreenActivity extends AppCompatActivity {
-
+    RadioGroup radioGroup1;
+    RadioGroup radioGroup2;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
     Button confirmButton;
     EditText inputTextP1;
     EditText inputTextP2;
     String s1;
     String s2;
+    int player1Color;
+    int player2Color;
+
 
 
     @Override
@@ -35,7 +43,8 @@ public class SetUpScreenActivity extends AppCompatActivity {
 
         inputTextP1 = (EditText) findViewById(R.id.inputTextP1);
         inputTextP2 = (EditText) findViewById(R.id.inputTextP2);
-
+        radioGroup1 = findViewById(R.id.radioGroup1);
+        radioGroup2 = findViewById(R.id.radioGroup2);
         confirmButton = findViewById(R.id.confirmButton);
         confirmButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -45,6 +54,12 @@ public class SetUpScreenActivity extends AppCompatActivity {
                 s2 = inputTextP2.getText().toString();
                 intent.putExtra("P1", s1);
                 intent.putExtra("P2", s2);
+               radioButton1 = findViewById(radioGroup1.getCheckedRadioButtonId());
+               player1Color = radioButton1.getCurrentTextColor();
+                radioButton2 = findViewById(radioGroup2.getCheckedRadioButtonId());
+                player2Color = radioButton2.getCurrentTextColor();
+                intent.putExtra("colorP1", player1Color);
+                intent.putExtra("colorP2", player2Color);
                 startActivity(intent);
                 finish();
 
@@ -54,10 +69,12 @@ public class SetUpScreenActivity extends AppCompatActivity {
 
     }
 
-    public void openMainActivity(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
- }
+
+
+
+
+
+
 }
 
 
